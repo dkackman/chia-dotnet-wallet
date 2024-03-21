0de143c771d41e2b1f7c2ce5065690a932749d7d
Don Kackman <dkackman@gmail.com>
2024-03-20 19:12:29 -0500

    add StandardWallet

--- src/wallet/AssetWallet.cs
+++ src/wallet/AssetWallet.cs
@@ -1,191 +1,190 @@
 using chia.dotnet.bls;
 using chia.dotnet.clvm;
 
-namespace chia.dotnet.wallet
+namespace chia.dotnet.wallet;
+
+/// <summary>
+/// Represents an asset wallet that manages asset tokens.
+/// </summary>
+public class AssetWallet : Wallet<AssetToken<StandardTransaction>>
 {
     /// <summary>
-    /// Represents an asset wallet that manages asset tokens.
+    /// Initializes a new instance of the <see cref="AssetWallet"/> class.
     /// </summary>
-    public class AssetWallet : Wallet<AssetToken<StandardTransaction>>
+    /// <param name="node">The full node proxy.</param>
+    /// <param name="keyStore">The key store.</param>
+    /// <param name="assetId">The asset ID.</param>
+    /// <param name="hiddenPuzzleHash">The hidden puzzle hash.</param>
+    /// <param name="walletOptions">The wallet options.</param>
+    public AssetWallet(
+        FullNodeProxy node,
+        KeyStore keyStore,
+        byte[] assetId,
+        byte[]? hiddenPuzzleHash = null,
+        WalletOptions? walletOptions = null
+    ) : base(node, keyStore, walletOptions)
     {
-        /// <summary>
-        /// Initializes a new instance of the <see cref="AssetWallet"/> class.
-        /// </summary>
-        /// <param name="node">The full node proxy.</param>
-        /// <param name="keyStore">The key store.</param>
-        /// <param name="assetId">The asset ID.</param>
-        /// <param name="hiddenPuzzleHash">The hidden puzzle hash.</param>
-        /// <param name="walletOptions">The wallet options.</param>
-        public AssetWallet(
-            FullNodeProxy node,
-            KeyStore keyStore,
-            byte[] assetId,
-            byte[]? hiddenPuzzleHash = null,
-            WalletOptions? walletOptions = null
-        ) : base(node, keyStore, walletOptions)
-        {
-            AssetId = assetId;
-            HiddenPuzzleHash = hiddenPuzzleHash ?? Puzzles.GetPuzzle("defaultHidden").Hash();
-        }
+        AssetId = assetId;
+        HiddenPuzzleHash = hiddenPuzzleHash ?? Puzzles.GetPuzzle("defaultHidden").Hash();
+    }
 
-        /// <summary>
-        /// Gets the asset ID.
-        /// </summary>
-        public byte[] AssetId { get; init; }
+    /// <summary>
+    /// Gets the asset ID.
+    /// </summary>
+    public byte[] AssetId { get; init; }
 
-        /// <summary>
-        /// Gets the hidden puzzle hash.
-        /// </summary>
-        public byte[] HiddenPuzzleHash { get; init; }
+    /// <summary>
+    /// Gets the hidden puzzle hash.
+    /// </summary>
+    public byte[] HiddenPuzzleHash { get; init; }
 
-        private Program? Tail { get; set; } = null;
+    private Program? Tail { get; set; } = null;
 
-        /// <summary>
-        /// Creates a puzzle for the specified key pair.
-        /// </summary>
-        /// <param name="keyPair">The key pair.</param>
-        /// <returns>The created asset token puzzle.</returns>
-        public override AssetToken<StandardTransaction> CreatePuzzle(KeyPair keyPair) =>
-            new AssetToken<StandardTransaction>(
-                AssetId,
-                new StandardTransaction(
-                    KeyDerivation.CalculateSyntheticPublicKey(
-                        keyPair.PublicKey,
-                        HiddenPuzzleHash
-                    )
+    /// <summary>
+    /// Creates a puzzle for the specified key pair.
+    /// </summary>
+    /// <param name="keyPair">The key pair.</param>
+    /// <returns>The created asset token puzzle.</returns>
+    public override AssetToken<StandardTransaction> CreatePuzzle(KeyPair keyPair) =>
+        new(
+            AssetId,
+            new StandardTransaction(
+                KeyDerivation.CalculateSyntheticPublicKey(
+                    keyPair.PublicKey,
+                    HiddenPuzzleHash
                 )
-            );
+            )
+        );
 
-        /// <summary>
-        /// Gets the parent coin spend for the specified coin record.
-        /// </summary>
-        /// <param name="coinRecord">The coin record.</param>
-        /// <param name="cancellationToken">The cancellation token.</param>
-        /// <returns>The parent coin spend.</returns>
-        public async Task<CoinSpend> GetParentCoinSpend(CoinRecord coinRecord, CancellationToken cancellationToken = default)
+    /// <summary>
+    /// Gets the parent coin spend for the specified coin record.
+    /// </summary>
+    /// <param name="coinRecord">The coin record.</param>
+    /// <param name="cancellationToken">The cancellation token.</param>
+    /// <returns>The parent coin spend.</returns>
+    public async Task<CoinSpend> GetParentCoinSpend(CoinRecord coinRecord, CancellationToken cancellationToken = default)
+    {
+        var parentResult = await Node.GetCoinRecordByName(coinRecord.Coin.ParentCoinInfo, cancellationToken);
+
+        var parentCoinSpendResult = await Node.GetPuzzleAndSolution(
+            coinRecord.Coin.ParentCoinInfo,
+            parentResult.SpentBlockIndex,
+            cancellationToken
+        );
+
+        return parentCoinSpendResult;
+    }
+
+    /// <summary>
+    /// Finds the tail of the asset token.
+    /// </summary>
+    /// <param name="cancellationToken">The cancellation token.</param>
+    /// <returns>The tail of the asset token.</returns>
+    public async Task<Program?> FindTail(CancellationToken cancellationToken = default)
+    {
+        if (Tail is not null)
         {
-            var parentResult = await Node.GetCoinRecordByName(coinRecord.Coin.ParentCoinInfo, cancellationToken);
-
-            var parentCoinSpendResult = await Node.GetPuzzleAndSolution(
-                coinRecord.Coin.ParentCoinInfo,
-                parentResult.SpentBlockIndex,
-                cancellationToken
-            );
-
-            return parentCoinSpendResult;
+            return Tail;
         }
 
-        /// <summary>
-        /// Finds the tail of the asset token.
-        /// </summary>
-        /// <param name="cancellationToken">The cancellation token.</param>
-        /// <returns>The tail of the asset token.</returns>
-        public async Task<Program?> FindTail(CancellationToken cancellationToken = default)
+        var coinRecord = CoinRecords.SelectMany(x => x).FirstOrDefault();
+
+        if (coinRecord is null)
         {
-            if (Tail is not null)
+            return null;
+        }
+
+        while (true)
+        {
+            var eveCoinRecord = coinRecord;
+
+            coinRecord = await Node.GetCoinRecordByName(coinRecord.Coin.ParentCoinInfo, cancellationToken);
+
+            var coinSpend = await Node.GetPuzzleAndSolution(coinRecord.Coin.CoinId.ToHex(), coinRecord.SpentBlockIndex, cancellationToken);
+
+            var puzzle = Program.DeserializeHex(HexHelper.SanitizeHex(coinSpend.PuzzleReveal));
+
+            var uncurriedPuzzle = puzzle.Uncurry();
+            if (uncurriedPuzzle is null)
             {
+                continue;
+            }
+
+            var puzzleMod = uncurriedPuzzle.Item1;
+            if (puzzleMod.Equals(Puzzles.GetPuzzle("cat")))
+            {
+                continue;
+            }
+            if (eveCoinRecord.SpentBlockIndex == 0)
+            {
+                continue;
+            }
+
+            var eveCoinSpend = await Node.GetPuzzleAndSolution(eveCoinRecord.Coin.CoinId.ToHex(), eveCoinRecord.SpentBlockIndex, cancellationToken);
+
+            var evePuzzle = Program.DeserializeHex(HexHelper.SanitizeHex(eveCoinSpend.PuzzleReveal));
+
+            var uncurriedEvePuzzle = evePuzzle.Uncurry();
+            if (uncurriedEvePuzzle == null || !uncurriedEvePuzzle.Item1.Equals(Puzzles.GetPuzzle("cat")))
+                throw new Exception("Eve is not an asset token.");
+
+            var result = uncurriedEvePuzzle.Item2.ToList()[2].Run(Program.Nil).Value;
+
+            if (result.IsAtom) throw new Exception("Asset spend output is atom.");
+
+            var conditions = result.ToList();
+
+            foreach (var condition in conditions)
+            {
+                if (condition.IsAtom)
+                {
+                    continue;
+                }
+                var args = condition.ToList();
+
+                if (args.Count < 5)
+                {
+                    continue;
+                }
+                if (args[0].IsCons || args[0].ToBigInt() != 51)
+                {
+                    continue;
+                }
+                if (args[2].IsCons || args[2].ToBigInt() != -113)
+                {
+                    continue;
+                }
+
+                Tail = args[3];
+
                 return Tail;
             }
 
-            var coinRecord = CoinRecords.SelectMany(x => x).FirstOrDefault();
-
-            if (coinRecord is null)
-            {
-                return null;
-            }
-
-            while (true)
-            {
-                var eveCoinRecord = coinRecord;
-
-                coinRecord = await Node.GetCoinRecordByName(coinRecord.Coin.ParentCoinInfo, cancellationToken);
-
-                var coinSpend = await Node.GetPuzzleAndSolution(coinRecord.Coin.CoinId.ToHex(), coinRecord.SpentBlockIndex, cancellationToken);
-
-                var puzzle = Program.DeserializeHex(HexHelper.SanitizeHex(coinSpend.PuzzleReveal));
-
-                var uncurriedPuzzle = puzzle.Uncurry();
-                if (uncurriedPuzzle is null)
-                {
-                    continue;
-                }
-
-                var puzzleMod = uncurriedPuzzle.Item1;
-                if (puzzleMod.Equals(Puzzles.GetPuzzle("cat")))
-                {
-                    continue;
-                }
-                if (eveCoinRecord.SpentBlockIndex == 0)
-                {
-                    continue;
-                }
-
-                var eveCoinSpend = await Node.GetPuzzleAndSolution(eveCoinRecord.Coin.CoinId.ToHex(), eveCoinRecord.SpentBlockIndex, cancellationToken);
-
-                var evePuzzle = Program.DeserializeHex(HexHelper.SanitizeHex(eveCoinSpend.PuzzleReveal));
-
-                var uncurriedEvePuzzle = evePuzzle.Uncurry();
-                if (uncurriedEvePuzzle == null || !uncurriedEvePuzzle.Item1.Equals(Puzzles.GetPuzzle("cat")))
-                    throw new Exception("Eve is not an asset token.");
-
-                var result = uncurriedEvePuzzle.Item2.ToList()[2].Run(Program.Nil).Value;
-
-                if (result.IsAtom) throw new Exception("Asset spend output is atom.");
-
-                var conditions = result.ToList();
-
-                foreach (var condition in conditions)
-                {
-                    if (condition.IsAtom)
-                    {
-                        continue;
-                    }
-                    var args = condition.ToList();
-
-                    if (args.Count < 5)
-                    {
-                        continue;
-                    }
-                    if (args[0].IsCons || args[0].ToBigInt() != 51)
-                    {
-                        continue;
-                    }
-                    if (args[2].IsCons || args[2].ToBigInt() != -113)
-                    {
-                        continue;
-                    }
-
-                    Tail = args[3];
-
-                    return Tail;
-                }
-
-                break;
-            }
-
-            throw new Exception("Coin record is not a genesis.");
+            break;
         }
 
-        /// <summary>
-        /// Signs the spend bundle with the private keys in the key store.
-        /// </summary>
-        /// <param name="spendBundle">The spend bundle to sign.</param>
-        /// <param name="aggSigMeExtraData">The aggregated signature me extra data.</param>
-        /// <returns>The signed spend bundle.</returns>
-        public override SpendBundle SignSpend(SpendBundle spendBundle, byte[] aggSigMeExtraData)
+        throw new Exception("Coin record is not a genesis.");
+    }
+
+    /// <summary>
+    /// Signs the spend bundle with the private keys in the key store.
+    /// </summary>
+    /// <param name="spendBundle">The spend bundle to sign.</param>
+    /// <param name="aggSigMeExtraData">The aggregated signature me extra data.</param>
+    /// <returns>The signed spend bundle.</returns>
+    public override SpendBundle SignSpend(SpendBundle spendBundle, byte[] aggSigMeExtraData)
+    {
+        var keysWithPrivate = KeyStore.Keys.Where(keyPair => keyPair.PrivateKey != null);
+        var syntheticPrivateKeys = keysWithPrivate.Select(keyPair => KeyDerivation.CalculateSyntheticPrivateKey(keyPair.PrivateKey!, HiddenPuzzleHash)).ToList();
+
+        if (KeyStore.PrivateKey is not null)
         {
-            var keysWithPrivate = KeyStore.Keys.Where(keyPair => keyPair.PrivateKey != null);
-            var syntheticPrivateKeys = keysWithPrivate.Select(keyPair => KeyDerivation.CalculateSyntheticPrivateKey(keyPair.PrivateKey!, HiddenPuzzleHash)).ToList();
-
-            if (KeyStore.PrivateKey is not null)
-            {
-                syntheticPrivateKeys.Add(KeyStore.PrivateKey);
-            }
-
-            var privateKeys = keysWithPrivate.Select(item => item.PrivateKey);
-            syntheticPrivateKeys.AddRange(privateKeys!);
-
-            return Sign.SignSpendBundle(spendBundle, aggSigMeExtraData, true, [.. syntheticPrivateKeys]);
+            syntheticPrivateKeys.Add(KeyStore.PrivateKey);
         }
+
+        var privateKeys = keysWithPrivate.Select(item => item.PrivateKey);
+        syntheticPrivateKeys.AddRange(privateKeys!);
+
+        return Sign.SignSpendBundle(spendBundle, aggSigMeExtraData, true, [.. syntheticPrivateKeys]);
     }
 }
---
+++ src/wallet/StandardWallet.cs
@@ -0,0 +1,142 @@
+using System.Numerics;
+using chia.dotnet.bls;
+using chia.dotnet.clvm;
+
+namespace chia.dotnet.wallet;
+
+/// <summary>
+/// Represents a standard wallet in the Chia.NET Wallet library.
+/// </summary>
+public class StandardWallet(
+        FullNodeProxy node,
+        KeyStore keyStore,
+        byte[]? hiddenPuzzleHash = null,
+        WalletOptions? walletOptions = null
+    ) : Wallet<StandardTransaction>(node, keyStore, walletOptions)
+{
+    /// <summary>
+    /// Gets the hidden puzzle hash.
+    /// </summary>
+    public byte[] HiddenPuzzleHash { get; } = hiddenPuzzleHash ?? Puzzles.GetPuzzle("defaultHidden").Hash();
+
+    /// <summary>
+    /// Sends a fee transaction.
+    /// </summary>
+    /// <param name="amount">The amount to send as a fee.</param>
+    /// <param name="cancellationToken">The cancellation token.</param>
+    /// <returns>A list of coin spends.</returns>
+    public async Task<List<CoinSpend>> SendFee(long amount, CancellationToken cancellationToken = default)
+    {
+        var coinRecords = SelectCoinRecords(amount, CoinSelection.Oldest);
+
+        var spendAmount = BigInteger.Zero;
+        foreach (var coinRecord in coinRecords)
+        {
+            spendAmount += coinRecord.Coin.Amount;
+        }
+
+        var change = PuzzleCache[(await FindUnusedIndices(1, [], cancellationToken: cancellationToken))[0]];
+
+        var puzzles = coinRecords.Select(coinRecord => PuzzleCache[CoinRecordIndex(coinRecord)]).ToList();
+
+        var coinSpends = coinRecords.Select((record, i) =>
+        {
+            var puzzle = puzzles[i];
+
+            var conditions = new List<Program>();
+
+            if (i == 0)
+            {
+                if (spendAmount > amount)
+                {
+                    conditions.Add(Program.FromSource($"(51 {HexHelper.FormatHex(change.HashHex())} {spendAmount - amount})"));
+                }
+            }
+
+            return puzzle.Spend(record.Coin, StandardTransaction.GetSolution(conditions));
+        }).ToList();
+
+        return coinSpends;
+    }
+
+    /// <summary>
+    /// Sends a transaction.
+    /// </summary>
+    /// <param name="puzzleHash">The puzzle hash.</param>
+    /// <param name="amount">The amount to send.</param>
+    /// <param name="fee">The fee amount.</param>
+    /// <param name="cancellationToken">The cancellation token.</param>
+    /// <returns>A list of coin spends.</returns>
+    public async Task<List<CoinSpend>> Send(byte[] puzzleHash, long amount, long fee, CancellationToken cancellationToken = default)
+    {
+        var totalAmount = amount + fee;
+
+        var coinRecords = SelectCoinRecords(totalAmount, CoinSelection.Oldest);
+
+        var spendAmount = BigInteger.Zero;
+        foreach (var coinRecord in coinRecords)
+        {
+            spendAmount += coinRecord.Coin.Amount;
+        }
+
+        var change = PuzzleCache[(await FindUnusedIndices(1, [], cancellationToken: cancellationToken))[0]];
+
+        var puzzles = coinRecords.Select(coinRecord => PuzzleCache[CoinRecordIndex(coinRecord)]).ToList();
+
+        var coinSpends = coinRecords.Select((record, i) =>
+        {
+            var puzzle = puzzles[i];
+
+            var conditions = new List<Program>();
+
+            if (i == 0)
+            {
+                conditions.Add(Program.FromSource($"(51 {HexHelper.FormatHex(puzzleHash.ToHex())} {amount})"));
+
+                if (spendAmount > totalAmount)
+                {
+                    conditions.Add(Program.FromSource($"(51 {HexHelper.FormatHex(change.HashHex())} {spendAmount - totalAmount})"));
+                }
+            }
+
+            return puzzle.Spend(record.Coin, StandardTransaction.GetSolution(conditions));
+        }).ToList();
+
+        return coinSpends;
+    }
+
+    /// <summary>
+    /// Creates a puzzle for the specified key pair.
+    /// </summary>
+    /// <param name="keyPair">The key pair.</param>
+    /// <returns>A new instance of the <see cref="StandardTransaction"/> class.</returns>
+    public override StandardTransaction CreatePuzzle(KeyPair keyPair) => new(KeyDerivation.CalculateSyntheticPublicKey(keyPair.PublicKey, HiddenPuzzleHash));
+
+    /// <summary>
+    /// Signs a spend bundle with the specified aggregated signature me extra data.
+    /// </summary>
+    /// <param name="spendBundle">The spend bundle to sign.</param>
+    /// <param name="aggSigMeExtraData">The aggregated signature me extra data.</param>
+    /// <returns>The signed spend bundle.</returns>
+    public override SpendBundle SignSpend(SpendBundle spendBundle, byte[] aggSigMeExtraData)
+    {
+        var syntheticPrivateKeys = KeyStore.Keys
+            .Where(keyPair => keyPair.PrivateKey != null)
+            .Select(keyPair => KeyDerivation.CalculateSyntheticPrivateKey(keyPair.PrivateKey!, HiddenPuzzleHash))
+            .ToList();
+
+        if (KeyStore.PrivateKey != null)
+        {
+            syntheticPrivateKeys.Add(KeyStore.PrivateKey);
+        }
+
+        var privateKeys = KeyStore.Keys
+            .Where(keyPair => keyPair.PrivateKey != null)
+            .Select(item => item.PrivateKey)
+            .ToList();
+
+        syntheticPrivateKeys.AddRange(privateKeys!);
+
+        return Sign.SignSpendBundle(spendBundle, aggSigMeExtraData, true, [.. syntheticPrivateKeys]);
+    }
+}
